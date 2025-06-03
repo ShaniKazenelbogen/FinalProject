0044c5d2539ceccfb82b0cb2064aa7c9203326fe
RivkyHoff <rivky7781@gmail.com>
2025-06-03 17:25:43 +0300

    Adding therapist functions

---
+++ TherapyCenter/Controllers/TherapistCntroller.cs
@@ -0,0 +1,50 @@
+﻿using Bl;
+using Bl.API;
+using Dal.models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TherapyCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Therapist")]
+    public class TherapistController : ControllerBase
+    {
+        private readonly IBlTherapistServices _blTherapistServices;
+
+        public TherapistController(IBlTherapistServices blTherapistServices)
+        {
+            _blTherapistServices = blTherapistServices;
+        }
+
+        [HttpGet("appointments/{date}")]
+        public IActionResult GetAppointmentsByDate(DateTime date)
+        {
+            var appointments = _blTherapistServices.GetAllAppointmentsForTheSpecificDate(date);
+            if (appointments == null || !appointments.Any())
+            {
+                return NotFound();
+            }
+            return Ok(appointments);
+        }
+        [HttpGet("upcoming")]
+        public IActionResult GetUpcomingClients()
+        {
+            var today = DateTime.Today;
+            var tomorrow = today.AddDays(1);
+            var upcomingClients = _blTherapistServices.GetClientsForTodayAndTomorrow();
+
+            if (upcomingClients == null || !upcomingClients.Any())
+            {
+                return NotFound();
+            }
+            return Ok(upcomingClients);
+        }
+    }
+}
+
+   
